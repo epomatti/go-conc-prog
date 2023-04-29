11737bd4f9a81465b7ea65de5fd6d6298a3d6177
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-04-29 00:58:41 -0300

    context done

--- extra/contexts/main.go
+++ extra/contexts/main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"sync"
 	"time"
 )
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go func() {
+	go func(ctx context.Context) {
+		defer wg.Done()
 		for range time.Tick(500 * time.Millisecond) {
+			if ctx.Err() != nil {
+				log.Println(ctx.Err())
+				return
+			}
 			fmt.Println("Tick!")
 		}
-		wg.Done()
-	}()
+	}(ctx)
+
+	time.Sleep(2 * time.Second)
+	cancel()
 
 	wg.Wait()
 }
